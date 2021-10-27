e3cead2b0d26707a8f193febfb863d2bc719ca9c
Ashwin Naik <ashwinknaik@google.com>
2021-10-27 12:40:17 +0530

    added lb

---
+++ lb.tf
@@ -0,0 +1,44 @@
+resource "google_compute_forwarding_rule" "default1" {
+  name                  = "website-forwarding-rule1"
+  region                = "us-central1"
+  load_balancing_scheme = "INTERNAL"
+  backend_service       = google_compute_region_backend_service.backend.id
+  all_ports             = true
+  allow_global_access   = true
+  network               = google_compute_network.default.name
+  subnetwork            = google_compute_subnetwork.default.name
+}
+
+resource "google_compute_forwarding_rule" "default2" {
+  name                  = "website-forwarding-rule2"
+  region                = "us-central1"
+  load_balancing_scheme = "INTERNAL"
+  backend_service       = google_compute_region_backend_service.backend.id
+  allow_global_access   = true
+  network               = google_compute_network.default.name
+  subnetwork            = google_compute_subnetwork.default.name
+}
+
+resource "google_compute_region_backend_service" "backend" {
+  name                  = "website-backend"
+  region                = "us-central1"
+  health_checks         = [google_compute_health_check.hc.id]
+}
+resource "google_compute_health_check" "hc" {
+  name               = "check-website-backend"
+  check_interval_sec = 1
+  timeout_sec        = 1
+  tcp_health_check {
+    port = "80"
+  }
+}
+resource "google_compute_network" "default" {
+  name = "website-net"
+  auto_create_subnetworks = false
+}
+resource "google_compute_subnetwork" "default" {
+  name          = "website-net"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.default.id
+}
