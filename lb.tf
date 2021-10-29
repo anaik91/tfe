41c71e501117aed8ea06741ee880b581488693e3
Ashwin Naik <ashwinknaik@google.com>
2021-10-29 12:26:05 +0530

    update

--- lb.tf
+++ lb.tf
@@ -7,6 +7,19 @@ resource "google_compute_forwarding_rule" "default1" {
   allow_global_access   = true
   network               = google_compute_network.default.name
   subnetwork            = google_compute_subnetwork.default.name
+  labels = {
+    gcp_region           = "US"
+    owner                = "hybridenv"
+    application_division = "pci"
+    application_name     = "demo"
+    application_role     = "app"
+    environment          = "dev"
+    au                   = "0223092"
+    data_confidentiality = "pub"
+    data_compliance      = "pci"
+    data_type            = "test"
+    created              = "YYYYMMDD"
+  }
 }
 
 resource "google_compute_forwarding_rule" "default2" {
@@ -41,4 +54,9 @@ resource "google_compute_subnetwork" "default" {
   ip_cidr_range = "10.0.0.0/16"
   region        = "us-central1"
   network       = google_compute_network.default.id
+}
+
+resource "google_compute_region_backend_service" "backend_no_hc" {
+  name                  = "website-backend"
+  region                = "us-central1"
 }
