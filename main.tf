50f0499c6a4d20e3109f7c4cfa0144ce0efd2447
Ashwin Naik <ashwinknaik@google.com>
2021-11-23 12:45:34 +0530

    fix

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "google" {
-  access_token = var.access_token
-}
-
-provider "google-beta" {
-  access_token = var.access_token
-}
 resource "google_service_account" "default" {
   account_id   = "service-account-id"
   display_name = "Service Account"
--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "access_token" {
-  description = "access_token"
-  type        = string
-  sensitive   = true
-}
